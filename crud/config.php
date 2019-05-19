2a054fd6b8536d87ebcc9ae149b90f6710db7c33
dave6181989 <47545711+dave6181989@users.noreply.github.com>
2019-05-19 19:06:06 -0400

    Update config.php

--- crud/config.php
+++ crud/config.php
@@ -4,7 +4,7 @@ server with default setting (user 'root' with no password) */
 define('DB_SERVER', 'localhost');
 define('DB_USERNAME', 'root');
 define('DB_PASSWORD', '');
-define('DB_NAME', 'crud');
+define('DB_NAME', 'project');
  
 /* Attempt to connect to MySQL database */
 try{
